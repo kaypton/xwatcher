567d9f3699cab8ea821c0b91ebc72deee20063bc
AC-JO <psvaiai@163.com>
2022-01-11 17:39:10 +0800

    protobuf

--- XTraceAnomalyDetector/src/main/java/com/github/fenrir/xtraceanomalydetector/detectors/firm/CriticalPathExtractor.java
+++ XTraceAnomalyDetector/src/main/java/com/github/fenrir/xtraceanomalydetector/detectors/firm/CriticalPathExtractor.java
@@ -1,4 +1,11 @@
 package com.github.fenrir.xtraceanomalydetector.detectors.firm;
 
+import com.github.fenrir.xtraceanomalydetector.protobuf.Trace;
+
 public class CriticalPathExtractor {
+    public CriticalPathExtractor(){}
+
+    public void extract(Trace.TraceGraph graph){
+
+    }
 }
